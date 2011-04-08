a9ff0c2251202541aacf957957d8956bd521ebb7
Miroslav Mirkovic <mmirkovi@hsz-t.ch>
2011-04-08 16:38:48 +0200

    set some methods

--- src/ch/hszt/sp/views/CShortestPathView.java
+++ src/ch/hszt/sp/views/CShortestPathView.java
@@ -1,5 +1,59 @@
 package ch.hszt.sp.views;
 
-public class CShortestPathView {
+public class CShortestPathView implements IShortestPathListener, IShortestPathGui{
+
+	@Override
+	public void getNodePosition() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getEdge() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setNodePositions() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setEdges() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setShortestPath() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void calcDistanceBtn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void viewPathBtn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void selectNode() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void selectNodeBtn() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
