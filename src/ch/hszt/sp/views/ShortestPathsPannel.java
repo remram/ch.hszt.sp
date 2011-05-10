58c6c5a711e2214d2e9ae8e49e0b019ca19b424a
Miroslav Mirkovic <mmirkovi@hsz-t.ch>
2011-05-10 17:52:59 +0200

    Bugfixed Panel access on CNode.

--- src/ch/hszt/sp/views/ShortestPathsPannel.java
+++ src/ch/hszt/sp/views/ShortestPathsPannel.java
@@ -7,8 +7,7 @@ import ch.hszt.sp.models.CEdge;
 import ch.hszt.sp.models.CNode;
 
 import java.awt.*;
-import java.awt.Stroke;
-import java.awt.BasicStroke;
+import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -26,7 +25,7 @@ public class ShortestPathsPannel extends JPanel{
 	private static final long serialVersionUID = 1L;
 	private ArrayList<CNode> cnlist;
 	private ArrayList<CEdge> cEdge;
-	
+	private CNode cnoda, cnodb;
 	
 	//Der Konstruktor kann ein img path annehmen und dieses im Pannel darstellen.
 	public ShortestPathsPannel(List<CNode> cnlist, List<CEdge> cEdge){
@@ -38,6 +37,10 @@ public class ShortestPathsPannel extends JPanel{
 	public void paintComponent(Graphics g){
 		//g.drawImage(img,0,0,this);
 		
+		for(CEdge ceg : this.cEdge){
+			System.out.println(ceg.getId());
+		}
+		
 		try {
 			for (CNode cnode : this.cnlist) {
 				ShowNode shn = new ShowNode(cnode.getxCoordinate(), cnode.getyCoordinate(), cnode.getId(), cnode.getName());
@@ -47,12 +50,37 @@ public class ShortestPathsPannel extends JPanel{
 			e.printStackTrace();
 		}
 		
-		for (int i=0; i < cEdge.size() - 1; i++){
-			CEdge cedg = cEdge.get(i);
-			CNode cnoda = cnlist.get(cedg.getStartNode()-1);
-			CNode cnodb = cnlist.get(cedg.getTargetNode()-1);
+		for (CEdge cedg : this.cEdge){
+			for(CNode cnodq : this.cnlist){
+				if(cedg.getStartNode() == cnodq.getId()){
+					this.cnoda = cnodq;
+				}
+				if(cedg.getTargetNode() == cnodq.getId()){
+					this.cnodb = cnodq;
+				}
+		}
+			//CNode cnoda = cnlist.get(cedg.getStartNode()-1);
+			
+			/*Iterator<CNode> it = this.cnlist.iterator();
+			
+			while(it.hasNext()){
+				CNode cnod = it.next();
+				System.out.println(cnod.getId());
+				
+				if(cnod.getId() == cedg.getStartNode()){
+					this.cnoda = cnod;
+				}
+			}
+			while(it.hasNext()){
+				CNode cnod = it.next();
+				if(cnod.getId() == cedg.getTargetNode()){
+					this.cnodb = cnod;
+	        drawString
+				}
+			}*/
+			//CNode cnodb = cnlist.get(cedg.getTargetNode()-1);
 			System.out.println(cedg.getId() + " ist die Kante mit folgendem Startknoten: "+cedg.getStartNode() + " " +cnoda.getId() + " : "+cedg.getTargetNode());
-			ShowEdge she = new ShowEdge(cnoda.getxCoordinate()+5, cnoda.getyCoordinate()+5, cnodb.getxCoordinate()+5, cnodb.getyCoordinate()+5);
+			ShowEdge she = new ShowEdge(cnoda.getxCoordinate()+5, cnoda.getyCoordinate()+5, cnodb.getxCoordinate()+5, cnodb.getyCoordinate()+5, cedg);
 			she.paintComponent(g);
 		}
 		
@@ -104,8 +132,10 @@ public class ShortestPathsPannel extends JPanel{
 		 * */
 		private static final long serialVersionUID = 1L;
 		private final Stroke stroke = new BasicStroke(2.0F);
-		private int ax, ay, bx, by;
-		public ShowEdge(int ax, int ay, int bx, int by){
+		private int ax, ay, bx, by, weight;
+		
+		public ShowEdge(int ax, int ay, int bx, int by, CEdge cedge){
+			this.weight = (int) cedge.getWeight();
 			this.ax = ax;
 			this.ay = ay;
 			this.bx = bx;
@@ -114,10 +144,13 @@ public class ShortestPathsPannel extends JPanel{
 		}
 		
 		public void paintComponent(Graphics g){
+			String weight = ""+ this.weight;
 			Graphics2D g2d = (Graphics2D) g;
 			g2d.setStroke(stroke);
 	        g2d.setColor(Color.BLUE);
-	        g2d.drawLine(ax, ay, bx, by);			
+	        g2d.drawLine(ax, ay, bx, by);	
+	        g2d.setColor(Color.gray);
+	        //g2d.drawString(weight, ax, bx+50);
 			}
 		}
 
