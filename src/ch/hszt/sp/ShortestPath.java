c0e5d4d2411f92f07106c1f38392f4554cdcd4ed
remram <xview28@gmail.com>
2011-06-06 20:12:47 +0200

    We do use ShortestPath instead of SPController class

--- src/ch/hszt/sp/ShortestPath.java
+++ src/ch/hszt/sp/ShortestPath.java
@@ -1,5 +1,21 @@
 package ch.hszt.sp;
 
+import ch.hszt.sp.controllers.CShortestPathController;
+import ch.hszt.sp.exceptions.DataAccessException;
+
+/**
+ * @author Ramy Hasan, Miroslav Mirković
+ * @since 08.05.11
+ * Der SPController Instanziert die fuer unser Programm benoetigten Klassen.
+ * 
+ **/
 public class ShortestPath {
+	
+
+	public static void main(String[] args) throws DataAccessException {
+		new CShortestPathController();
+		
+		
+	}
 
 }
