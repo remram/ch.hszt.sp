64e05fcfaf9b8a4a81bb0592dea2d2c4ab83e329
Miroslav Mirkovic <mmirkovi@hsz-t.ch>
2011-06-17 21:55:07 +0200

    added jtable

--- src/ch/hszt/sp/views/ShowEdge.java
+++ src/ch/hszt/sp/views/ShowEdge.java
@@ -4,7 +4,6 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Polygon;
 import java.awt.Stroke;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -18,7 +17,7 @@ import ch.hszt.sp.models.CNode;
 public class ShowEdge extends JPanel
 {
 	/**
-	 * @author miro
+	 * @author Miroslav Mirkovic
 	 * @version 1.0
 	 * Die Klasse ShowEdge ist fuer die Dartsellung der Kanten zustaendig.
 	 * 
